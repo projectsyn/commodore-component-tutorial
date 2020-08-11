7ea8a4a6c71d076a3757e34f769e7f2f2b7ef178
Adrian Kosmaczewski <adrian@vshn.ch>
2020-08-11 11:20:11 +0200

    A more readable command

--- commodore.sh
+++ commodore.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
 commodore () {
-    docker run --env-file=.env --interactive=true --tty --rm --user="$(id -u)" --volume ~/.ssh:/app/.ssh:ro --volume "$PWD"/compiled/:/app/compiled/ --volume "$PWD"/catalog/:/app/catalog --volume "$PWD"/dependencies/:/app/dependencies/ --volume "$PWD"/inventory/:/app/inventory/ --volume ~/.gitconfig:/app/.gitconfig:ro projectsyn/commodore:v0.2.0 $*
+    docker run \
+    --env-file=.env \
+    --interactive=true \
+    --tty \
+    --rm \
+    --user="$(id -u)" \
+    --volume "$HOME"/.ssh:/app/.ssh:ro \
+    --volume "$PWD"/compiled/:/app/compiled/ \
+    --volume "$PWD"/catalog/:/app/catalog \
+    --volume "$PWD"/dependencies/:/app/dependencies/ \
+    --volume "$PWD"/inventory/:/app/inventory/ \
+    --volume ~/.gitconfig:/app/.gitconfig:ro \
+    projectsyn/commodore:v0.2.0 \
+    "$*"
 }
