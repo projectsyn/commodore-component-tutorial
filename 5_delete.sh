5833a80c21ce9168b723d1f626b9c5071c3bedf6
Adrian Kosmaczewski <adrian@vshn.ch>
2020-08-27 09:22:23 +0200

    Changed delete script to remove all clusters

--- 5_delete.sh
+++ 5_delete.sh
@@ -3,25 +3,17 @@
 source lib/functions.sh
 source lib/minikube.sh
 
-check_minikube
-check_variable "GITLAB_ENDPOINT" $GITLAB_ENDPOINT
-check_variable "GITLAB_USERNAME" $GITLAB_USERNAME
-
 echo "===> Find Tenant ID"
 TENANT_ID=$(kubectl --context minikube -n lieutenant get tenant | grep t- | awk 'NR==1{print $1}')
 check_variable "TENANT_ID" $TENANT_ID
 
-echo "===> Find Minikube Cluster ID"
-MINIKUBE_CLUSTER_ID=$(kubectl --context minikube -n lieutenant get cluster | grep c- | grep Minikube | awk 'NR==1{print $1}')
-check_variable "MINIKUBE_CLUSTER_ID" $MINIKUBE_CLUSTER_ID
+echo "===> Removing all clusters"
+CLUSTERS=($(kubectl --context minikube -n lieutenant get cluster -o jsonpath="{$.items[*].metadata.name}"))
+for CLUSTER in "${CLUSTERS[@]}"; do
+    kubectl --context minikube -n lieutenant delete cluster "$CLUSTER"
+done
 
-echo "===> Find K3s Cluster ID"
-K3S_CLUSTER_ID=$(kubectl --context minikube -n lieutenant get cluster | grep c- | grep K3s | awk 'NR==1{print $1}')
-check_variable "K3S_CLUSTER_ID" $K3S_CLUSTER_ID
-
-echo "===> Removing everything"
-kubectl --context minikube -n lieutenant delete cluster "$K3S_CLUSTER_ID"
-kubectl --context minikube -n lieutenant delete cluster "$MINIKUBE_CLUSTER_ID"
+echo "===> Removing tenant"
 kubectl --context minikube -n lieutenant delete tenant "$TENANT_ID"
 
 echo "===> Waiting 20 seconds for the removal of GitLab repositories"
@@ -29,4 +21,5 @@ sleep 20s
 
 minikube delete
 k3d cluster delete projectsyn
+kind delete cluster --name projectsyn
 killall ngrok
