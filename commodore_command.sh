8acb838dadffa0f7c89f42cdaa57424ff2078fb5
Adrian Kosmaczewski <adrian@vshn.ch>
2020-08-27 09:48:57 +0200

    Added echo with information

--- commodore_command.sh
+++ commodore_command.sh
@@ -20,6 +20,7 @@ commodore () {
 commodore_compile_all() {
     CLUSTERS=($(kubectl --context minikube -n lieutenant get cluster -o jsonpath="{$.items[*].metadata.name}"))
     for CLUSTER in "${CLUSTERS[@]}"; do
+        echo "===> Compiling and pushing catalog for cluster $CLUSTER"
         commodore catalog compile --push "$CLUSTER"
     done
 }
