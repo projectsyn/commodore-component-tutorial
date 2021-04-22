7f5dea0b15d5452c3ee98c3daab4ae25c1c8b415
Adrian Kosmaczewski <adrian.kosmaczewski@vshn.ch>
2021-04-22 14:26:01 +0200

    Trying to add support for k8s in Exoscale

---
+++ 4_synthesize_on_k8s.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+source lib/functions.sh
+
+check_variable "GITLAB_TOKEN" $GITLAB_TOKEN
+check_variable "GITLAB_ENDPOINT" $GITLAB_ENDPOINT
+check_variable "GITLAB_USERNAME" $GITLAB_USERNAME
+check_variable "TENANT_ID" $TENANT_ID
+check_variable "LIEUTENANT_URL" $LIEUTENANT_URL
+check_variable "LIEUTENANT_TOKEN" $LIEUTENANT_TOKEN
+check_variable "COMMODORE_SSH_PRIVATE_KEY" $COMMODORE_SSH_PRIVATE_KEY
+
+LIEUTENANT_AUTH="Authorization: Bearer ${LIEUTENANT_TOKEN}"
+
+echo "===> Register this cluster via the API"
+CLUSTER_ID=$(curl -s -H "$LIEUTENANT_AUTH" -H "Content-Type: application/json" -X POST --data "{ \"tenant\": \"${TENANT_ID}\", \"displayName\": \"Generic Kubernetes cluster\", \"facts\": { \"cloud\": \"exoscale\", \"distribution\": \"k3s\", \"region\": \"ch-gva-2\" }, \"gitRepo\": { \"url\": \"ssh://git@${GITLAB_ENDPOINT}/${GITLAB_USERNAME}/tutorial-cluster-k8s.git\" } }" "${LIEUTENANT_URL}/clusters" | jq -r ".id")
+check_variable "CLUSTER_ID" $CLUSTER_ID
+
+echo "===> Kickstart Commodore"
+echo "===> IMPORTANT: When prompted enter your SSH key password"
+kubectl --context minikube -n lieutenant run commodore-shell \
+  --image=docker.io/projectsyn/commodore:v0.6.0 \
+  --env=COMMODORE_API_URL="$LIEUTENANT_URL" \
+  --env=COMMODORE_API_TOKEN="$LIEUTENANT_TOKEN" \
+  --env=SSH_PRIVATE_KEY="$(cat ${COMMODORE_SSH_PRIVATE_KEY})" \
+  --env=CLUSTER_ID="$CLUSTER_ID" \
+  --env=GITLAB_ENDPOINT="$GITLAB_ENDPOINT" \
+  --tty --stdin --restart=Never --rm --wait \
+  --image-pull-policy=Always \
+  --command \
+  -- /usr/local/bin/entrypoint.sh bash -c "ssh-keyscan $GITLAB_ENDPOINT >> /app/.ssh/known_hosts; commodore catalog compile $CLUSTER_ID --push"
+
+echo "===> COMMODORE DONE"
+
+echo "===> Check the validity of the bootstrap token"
+wait_for_token "$CLUSTER_ID"
+
+echo "===> Retrieve the Steward install URL"
+STEWARD_INSTALL=$(curl --header "$LIEUTENANT_AUTH" --silent "${LIEUTENANT_URL}/clusters/${CLUSTER_ID}" | jq -r ".installURL")
+echo "===> Steward install URL: $STEWARD_INSTALL"
+
+echo "===> Install Steward in the Kubernetes cluster"
+kubectl --kubeconfig demo.kubeconfig apply -f "$STEWARD_INSTALL"
+
+echo "===> Check that Steward is running and that Argo CD Pods are appearing"
+kubectl --kubeconfig demo.kubeconfig -n syn get pod
+
+echo ""
+echo "===> STEWARD DONE"
